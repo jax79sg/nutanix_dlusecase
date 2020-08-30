bb604baf98ddbe146b97a4b15628fc7f253dfe8f
Jax <jax79sg@yahoo.com.sg>
2020-08-30 16:41:01 +0000

    Adjustments

--- setupVM.sh
+++ setupVM.sh
@@ -12,7 +12,8 @@ sudo apt-get install -y \
     gnupg-agent \
     software-properties-common \
     axel \
-    wget
+    wget \
+    zip
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88
 sudo add-apt-repository \
--- testTraining1gpu.sh
+++ testTraining1gpu.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo docker run --gpus all -d  -v $(pwd)/myconfig:/darknet/myconfig -v $(pwd)/mydata:/darknet/mydata -v $(pwd)/myweights:/darknet/myweights yolov4  /darknet/darknet detector train -dont_show /darknet/myconfig/coco.data /darknet/myconfig/yolov4.cfg /darknet/myweights/csdarknet53-omega.conv.105
+sudo docker run --gpus all -d --name testTraining1gpu -v $(pwd)/myconfig:/darknet/myconfig -v $(pwd)/mydata:/darknet/mydata -v $(pwd)/myweights:/darknet/myweights yolov4  /darknet/darknet detector train -dont_show /darknet/myconfig/coco.data /darknet/myconfig/yolov4.cfg /darknet/myweights/csdarknet53-omega.conv.105
 
